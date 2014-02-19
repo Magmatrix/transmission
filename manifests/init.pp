252d07d2cf0f441ebc3e587217359ca36b78d22b
Magmatrix <mattias@kregert.se>
2014-02-19 19:27:29 +0000

    better everything

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
-# = Class: transmission_daemon
+# = Class: transmission
 # 
-# This class installs/configures/manages the transmission-daemon bittorrent client. It can configure an RPC bittorrent client (web).
+# This class installs/configures/manages the transmission-daemon bittorrent client.
 # 
 # == Parameters: 
 #
-# $download_dir:: The directory where the files have to be downloaded. Defaults to +/var/lib/transmission-daemon/downloads+.
-# $incomplete_dir:: The temporary directory used to store incomplete files. The feature is disabled when the option is not set. By default this feature is disabled.
-# $rpc_url:: The access path to the RPC server (web). The feature is disabled when the option is not set. This path should not finish with the / (slash) char. By default this feature is disabled.
-# $rpc_port:: The port the RPC server is listening on. Defaults to +9091+.
-# $rpc_user:: The RPC user (ACL). Defaults to +transmission+.
-# $rpc_password:: The password of the RPC user (ACL). By default this option is not set.
-# $rpc_whitelist:: An array of IP addresses. This list define which machines are allowed to use the RPC interface. It is possible to use wildcards in the addresses. By default the list is empty.
-# $blocklist_url:: An url to a block list. By default this option is not set.
+# $download_dir:: The directory where the files have to be downloaded. Defaults to +$config_path/downloads+.
+# $incomplete_dir:: The temporary directory used to store incomplete files. Disabled when the option is not set (this is the default).
+# $web_path:: URL path for the web client (without trailing /). Disabled when the option is not set (this is the default).
+# $web_port:: The port the web server is listening on. Defaults to +9091+.
+# $web_user:: The web client login name. Defaults to +transmission+.
+# $web_password:: The password of the web client user (default: <none>)
+# $web_whitelist:: An array of IP addresses. This list define which machines are allowed to use the web interface. It is possible to use wildcards in the addresses. By default the list is empty.
+# $blocklist_url:: An url to a block list (default: <none>)
 #
 # == Requires: 
 # 
@@ -22,107 +22,112 @@
 #  class {'transmission':
 #    download_dir => "/downloads",
 #    incomplete_dir => "/tmp/downloads",
-#    rpc_url => "bittorrent/",
-#    rpc_port => 9091,
-#    rpc_whitelist => ['127.0.0.1'],
+#    web_path => "/bittorrent", ### Corrected
+#    web_port => 9091,
+#    web_whitelist => ['127.0.0.1'],
 #    blocklist_url => 'http://list.iblocklist.com/?list=bt_level1',
 #  }
 #
 
 class transmission (
-  $download_dir = '/downloads', ## TODO: extlookup('transmission:download_dir'),  ### NOT WORKING!!??!!
-  $incomplete_dir = undef,
-  $rpc_url = undef,
-  $rpc_port = 9091,
-  $rpc_user = 'transmission',
-  $rpc_password = undef,
-  $rpc_whitelist = undef,
-  $blocklist_url = undef,
   $config_path = undef,
+  $download_dir = '/downloads',
+  $incomplete_dir = undef,
+  $blocklist_url = undef,
+  $web_path = undef,
+  $web_port = 9091,
+  $web_user = 'transmission',
+  $web_password = undef,
+  $web_whitelist = undef,
 ) {
 
   package { 'transmission-daemon':
     ensure => installed,
   }
 
+  # Find out the name of the transmission user/group
+  case $operatingsystem {
+    'Debian','Ubuntu': { $transmission_ug='debian-transmission' }
+    default: { $transmission_ug='transmission' }
+  }
+
+  # Helper. To circumvent transmission's bad habit of rewriting 'settings.json' every now and then.
+  # Even tried to write protect settings.json, but no luck so far.
   exec { 'stop-daemon':
     command => 'service transmission-daemon stop',
     path    => ['/sbin', '/usr/sbin'],
     require => Package['transmission-daemon'], # Needs to be installed before we can try to stop it
   }
 
-  # Fix the settings // TEST, not complete
-  $input="${config_path}/settings.json"
-  $output="/tmp/transmission-modified-settings"
-  $my_download_dir="/TEST/MODIFIED/${download_dir}"
-  exec { 'Modify settings.json .....':
-    command => "sed 's|\"download-dir\":.*|\"download-dir\": \"${my_download_dir}\",|' ${input} > ${output}", # mv --force $outout $input
-    path    => ['/bin'],
-    require => File['settings.json'],
+  # Transmission should be able to read the config dir
+  file { "$config_path":
+    ensure   => directory,
+    group    => $transmission_ug, # We only set the group (the dir could be owned by root or someone ele)
+    mode     => 'g+rx',           # Make sure transmission can access the config dir
+    require  => Package["transmission-daemon"], # Make sure that the package had the opportunity to create the directory first
   }
 
+  # The settings file should follow our template
   file { 'settings.json':
-    path => "${config_path}/settings.json",
-    ensure => file,
-    require => Exec['stop-daemon'],
+    path    => "${config_path}/settings.json",
+    ensure  => file,
+    require => [File["$config_path"],Exec['stop-daemon']],
     content => template("${module_name}/settings.json.erb"),
     mode    => 'u+rw',          # Make sure transmisson can r/w settings
   }
 
-
-  # Create DL directory ...
-  # Big mess and a gazillion "Error 400" messages....
+  # Transmission should use the settings in ${config_path}/settings.json *only*
+  file {['/etc/default/transmission','/etc/default/transmission-daemon','/etc/sysconfig/transmission','/etc/sysconfig/transmission-daemon']:
+    ensure  => absent,                         # Kill the bastards
+    require => Package['transmission-daemon'], # The package has to be installed first. Otherwise this would be sheer folly.
+    before  => Service['transmission-daemon'], # After this is fixed, we can handle the service
+  }
+    
+  # Manage the download directory.  Creating parents will be taken care of "upstream" (in the calling class)
   file { "${download_dir}":
     ensure  => directory,
-    # recurse => true, ## Doesn't do what it sounds like (does NOT create parent diectories)
-    mode    => "ug+rw,u+x" # TODO: Should also check user and group (depends on distro)...
-    ## require => Package['transmission-daemon'], # TODO: Make sure it's installed before creating DL-dir, then check owner, make sure DL-dir has same owner/group
-  }
-  $parts=split($download_dir,"/")
-  $dir=""
-  ## $parts.each {|$part|  ## XXXXX: Documentation on puppetlabs is wrong!! ? WTF??!
-  each($parts) |$part| {
-    $dir="$dir/$part"           # XXXXX: THIS IS BROKEN! VALUE DOES NOT CHANGE! WTF!!??
-    if ($dir != '/') {
-      file { "$dir":
-        ensure => directory,
-        before => Notify["$dir"]
-      }
-      notify { "$dir":
-        message => "OK, '$dir' exists!"
-      }
-    }
+    recurse => true,
+    owner   => $transmission_ug,
+    group   => $transmission_ug,
+    mode    => "ug+rw,u+x",
+    require => Package['transmission-daemon'], # Let's give the installer a chance to create the directory and user before we manage this dir
   }
 
-  
+  # directory for partial downloads
   if $incomplete_dir {
     file { "${incomplete_dir}":
       ensure  => directory,
       recurse => true,
-      mode    => "ug+rw,u+x" # TODO: Should also check user and group (depends on distro)...
+      owner   => $transmission_ug,
+      group   => $transmission_ug,
+      mode    => "ug+rw,u+x",
+      require => Package['transmission-daemon'],
     }
   }
 
-
+  
   service { 'transmission-daemon':
     name => 'transmission-daemon',
     ensure => running,
     enable => true,
     hasrestart => true,
     hasstatus => true,
-    subscribe => File['settings.json'],
+    ## subscribe => File['settings.json','/etc/default/transmission','/etc/default/transmission-daemon','/etc/sysconfig/transmission','/etc/sysconfig/transmission-daemon'], # In case some evil guy added defaults and restarted transmission without teling us.
+    ## This is done "up there" instead (because: less typing, just a short "before"-clause)
   }
 
-  if $rpc_url and $blocklist_url {
-    if $rpc_password {
-      $opt_auth = " --auth ${rpc_user}:${rpc_password}"
+
+  # Keep blocklist updated
+  if $web_path and $blocklist_url {
+    if $web_password {
+      $opt_auth = " --auth ${web_user}:${web_password}"
     }
     else
     {
       $opt_auth = ""
     }
     cron { 'update-blocklist':
-      command => "/usr/bin/transmission-remote http://127.0.0.1:${rpc_port}${rpc_url}${opt_auth} --blocklist-update 2>&1 > /tmp/blocklist-update.log",
+      command => "/usr/bin/transmission-remote http://127.0.0.1:${web_port}${web_path}${opt_auth} --blocklist-update 2>&1 > /tmp/blocklist-update.log",
       user => root,
       hour => 2,
       minute => 0,
